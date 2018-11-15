34c10aa3b46215eaa837891ed77c23597509a821
Alena <golovina.helen@gmail.com>
2018-11-15 19:25:07 +0200

    minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
-'use strict';
-
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
+gulp.task('compress', function() {
+    gulp.src(['js/*.js'])
+        .pipe(concat('all.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'))
+});
 
 gulp.task('sass', function () {
     return gulp.src('./scss/style.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('default', function () {
